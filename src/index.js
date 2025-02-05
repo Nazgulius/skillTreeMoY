37ee2869893ed25f65a5387df3971975e65d6826
Maksim Chalyh <nazgulius@yandex.ru>
2025-02-05 09:13:16 +0300

    first in second commit

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ import './css/style.css';
 import './js/app';
 
 
+// Проверяем, какая страница загружена и загружаем соответствующий js с данными  
+window.onload = () => {  
+  const page = window.location.pathname.split('/').pop();  
+  if (page === 'mameWiz.html') {  
+    import('./js/listSkills.js').then(module => {  
+      displaySkills(module.default); // Загружаем и отображаем навыки  
+    });  
+  } else if (page === 'mageSage.html') {  
+    import('./js/skills2.js').then(module => {  
+      displaySkills(module.default); // Загружаем и отображаем навыки  
+    });  
+  }  
+};
