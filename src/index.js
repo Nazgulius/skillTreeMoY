617d75d2fde85a3194498b96053c90ab6b28927c
Maksim Chalyh <nazgulius@yandex.ru>
2025-02-07 22:40:16 +0300

    add create skill js, skills Sage, update app

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ import './css/style.css';
 import './js/app';
 
 
+/* 
+app.js -
+export function displaySkills(skills) {  
+  const content = document.getElementById('content');  
+  content.innerHTML = `<h2>Навыки:</h2><ul>${skills.map(skill => `<li>${skill}</li>`).join('')}</ul>`;  
+}
+
+index.js -
+import { displaySkills } from './app.js';  
+
+document.getElementById('page1Btn').addEventListener('click', () => {  
+  window.location.href = './html/page1.html'; // Переход на page1.html  
+});  
+
+document.getElementById('page2Btn').addEventListener('click', () => {  
+  window.location.href = './html/page2.html'; // Переход на page2.html  
+});  
+*/
+
 // Проверяем, какая страница загружена и загружаем соответствующий js с данными  
 window.onload = () => {  
   const page = window.location.pathname.split('/').pop();  
@@ -15,7 +34,7 @@ window.onload = () => {
       displaySkills(module.default); // Загружаем и отображаем навыки  
     });  
   } else if (page === 'mageSage.html') {  
-    import('./js/skills2.js').then(module => {  
+    import('./js/skillsSage.js').then(module => {  
       displaySkills(module.default); // Загружаем и отображаем навыки  
     });  
   }  
