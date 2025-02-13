d4d05564139efcd4e50700720958ff272048241a
Maksim Chalyh <nazgulius@yandex.ru>
2025-02-14 00:05:21 +0300

    update add.js, dep sword class

--- src/js/listSkills/Swordman.js
+++ src/js/listSkills/Swordman.js
@@ -57,7 +57,7 @@ export const skillsSwordman = [
     id: "bash",
     level: 0,
     dependencies: [],
-    dependent: [],
+    dependent: [{ id: "magnumBreak" }],
     element: null,
     skillName: "Bash",
     maxLevel: 10,
@@ -77,13 +77,40 @@ export const skillsSwordman = [
     [Lv 7]: 310% Damage, HIT +35%,
     [Lv 8]: 340% Damage, HIT +40%,
     [Lv 9]: 370% Damage, HIT +45%,
-    [Lv 10]: 400% Damage, HIT +50%`,
+    [Lv 10]: 400% Damage, HIT +50%
+    Formula: ATK%: 100 + (30 x Skill Lv)`,
+  },
+  {
+    id: "magnumBreak",
+    level: 0,
+    dependencies: [{ id: "bash", minLevel: 4 }],
+    dependent: [],
+    element: null,
+    skillName: "Magnum Break",
+    maxLevel: 10,
+    inform: `Max Lv: 10
+    Skill Form: Active
+    Type: Physical
+    Target: Self
+    Requirement: Bash Lv: 5
+    Description: Drain a small amount of the casters HP to inflict Fire property damage in a 7x7 area around the user and Knock Back them 2 cells backwards. The damage varies between the center 5x5 cells and the edges 7x7 cells. For 10 seconds per skill level after Magnum Break, casters weapon will receive a 20% Fire property strength enhancement.
+    [Lv 1]: Atk 120%, Edge: Atk 60%, HP Cost: 30,
+    [Lv 2]: Atk 140%, Edge: Atk 70%, HP Cost: 29,
+    [Lv 3]: Atk 160%, Edge: Atk 80%, HP Cost: 28,
+    [Lv 4]: Atk 180%, Edge: Atk 90%, HP Cost: 27,
+    [Lv 5]: Atk 200%, Edge: Atk 100%, HP Cost: 26,
+    [Lv 6]: Atk 220%, Edge: Atk 110%, HP Cost: 25,
+    [Lv 7]: Atk 240%, Edge: Atk 120%, HP Cost: 24,
+    [Lv 8]: Atk 260%, Edge: Atk 130%, HP Cost: 23,
+    [Lv 9]: Atk 280%, Edge: Atk 140%, HP Cost: 22,
+    [Lv 10]: Atk 300%, Edge: Atk 150%, HP Cost: 21
+    Formula: ATK%: 100 + (20 x Skill Lv)`,
   },
   {
     id: "provoke",
     level: 0,
     dependencies: [],
-    dependent: [],
+    dependent: [{ id: "endure" }],
     element: null,
     skillName: "Provoke",
     maxLevel: 10,
@@ -106,57 +133,9 @@ export const skillsSwordman = [
     [Lv 10]: Success Rate: 100%, Enemys Atk +32%, DEF -55%,`,
   },
   {
-    id: "increaseHPRecovery",
-    level: 0,
-    dependencies: [],
-    dependent: [],
-    element: null,
-    skillName: "Increase HP Recovery",
-    maxLevel: 10,
-    inform: `Max Lv: 10
-    Skill Form: Passive
-    Type: Misc
-    Requirement: None
-    Description: Increases the amount of HP recovered through your natural HP regeneration. Also boosts HP Potions healing in 3% per Skill Level.
-    [Lv 1]: HP Recovery +[5 + 0.1% of MaxHP],
-    [Lv 2]: HP Recovery +[10 + 0.2% of MaxHP],
-    [Lv 3]: HP Recovery +[15 + 0.3% of MaxHP],
-    [Lv 4]: HP Recovery +[20 + 0.4% of MaxHP],
-    [Lv 5]: HP Recovery +[25 + 0.5% of MaxHP],
-    [Lv 6]: HP Recovery +[30 + 0.6% of MaxHP],
-    [Lv 7]: HP Recovery +[35 + 0.7% of MaxHP],
-    [Lv 8]: HP Recovery +[40 + 0.8% of MaxHP],
-    [Lv 9]: HP Recovery +[45 + 0.9% of MaxHP],
-    [Lv 10]: HP Recovery +[50 + 1.0% of MaxHP]`,
-  },
-  {
-    id: "movingHPRecovery",
-    level: 0,
-    dependencies: [],
-    dependent: [],
-    element: null,
-    skillName: "Moving HP Recovery",
-    maxLevel: 10,
-    inform: `Max Lv: 10
-    Skill Form: Passive
-    Type: Misc
-    Requirement: Increase HP Recovery Lv: 5
-    Description: Enables natural HP regeneration while moving or fighting.
-    [Lv 1]: Natural recovery every 24 seconds.
-    [Lv 2]: Natural recovery every 22 seconds.
-    [Lv 3]: Natural recovery every 20 seconds.
-    [Lv 4]: Natural recovery every 18 seconds.
-    [Lv 5]: Natural recovery every 16 seconds.
-    [Lv 6]: Natural recovery every 14 seconds.
-    [Lv 7]: Natural recovery every 12 seconds.
-    [Lv 8]: Natural recovery every 10 seconds.
-    [Lv 9]: Natural recovery every 8 seconds.
-    [Lv 10]: Natural recovery every 6 seconds.`,
-  },
-  {
     id: "endure",
     level: 0,
-    dependencies: [],
+    dependencies: [{ id: "provoke", minLevel: 4 }],
     dependent: [],
     element: null,
     skillName: "Endure",
@@ -179,29 +158,52 @@ export const skillsSwordman = [
     [Lv 10]: DEF +50, CRIT DEF +10, Duration: 25 seconds,`,
   },
   {
-    id: "magnumBreak",
+    id: "increaseHPRecovery",
     level: 0,
     dependencies: [],
-    dependent: [],
+    dependent: [{ id: "movingHPRecovery" }],
     element: null,
-    skillName: "Magnum Break",
+    skillName: "Increase HP Recovery",
     maxLevel: 10,
     inform: `Max Lv: 10
-    Skill Form: Active
-    Type: Physical
-    Target: Self
-    Requirement: Bash Lv: 5
-    Description: Drain a small amount of the casters HP to inflict Fire property damage in a 7x7 area around the user and Knock Back them 2 cells backwards. The damage varies between the center 5x5 cells and the edges 7x7 cells. For 10 seconds per skill level after Magnum Break, casters weapon will receive a 20% Fire property strength enhancement.
-    [Lv 1]: Atk 120%, Edge: Atk 60%, HP Cost: 30,
-    [Lv 2]: Atk 140%, Edge: Atk 70%, HP Cost: 29,
-    [Lv 3]: Atk 160%, Edge: Atk 80%, HP Cost: 28,
-    [Lv 4]: Atk 180%, Edge: Atk 90%, HP Cost: 27,
-    [Lv 5]: Atk 200%, Edge: Atk 100%, HP Cost: 26,
-    [Lv 6]: Atk 220%, Edge: Atk 110%, HP Cost: 25,
-    [Lv 7]: Atk 240%, Edge: Atk 120%, HP Cost: 24,
-    [Lv 8]: Atk 260%, Edge: Atk 130%, HP Cost: 23,
-    [Lv 9]: Atk 280%, Edge: Atk 140%, HP Cost: 22,
-    [Lv 10]: Atk 300%, Edge: Atk 150%, HP Cost: 21`,
+    Skill Form: Passive
+    Type: Misc
+    Requirement: None
+    Description: Increases the amount of HP recovered through your natural HP regeneration. Also boosts HP Potions healing in 3% per Skill Level.
+    [Lv 1]: HP Recovery +[5 + 0.1% of MaxHP],
+    [Lv 2]: HP Recovery +[10 + 0.2% of MaxHP],
+    [Lv 3]: HP Recovery +[15 + 0.3% of MaxHP],
+    [Lv 4]: HP Recovery +[20 + 0.4% of MaxHP],
+    [Lv 5]: HP Recovery +[25 + 0.5% of MaxHP],
+    [Lv 6]: HP Recovery +[30 + 0.6% of MaxHP],
+    [Lv 7]: HP Recovery +[35 + 0.7% of MaxHP],
+    [Lv 8]: HP Recovery +[40 + 0.8% of MaxHP],
+    [Lv 9]: HP Recovery +[45 + 0.9% of MaxHP],
+    [Lv 10]: HP Recovery +[50 + 1.0% of MaxHP]`,
+  },
+  {
+    id: "movingHPRecovery",
+    level: 0,
+    dependencies: [{ id: "increaseHPRecovery", minLevel: 4 }],
+    dependent: [],
+    element: null,
+    skillName: "Moving HP Recovery",
+    maxLevel: 10,
+    inform: `Max Lv: 10
+    Skill Form: Passive
+    Type: Misc
+    Requirement: Increase HP Recovery Lv: 5
+    Description: Enables natural HP regeneration while moving or fighting.
+    [Lv 1]: Natural recovery every 24 seconds.
+    [Lv 2]: Natural recovery every 22 seconds.
+    [Lv 3]: Natural recovery every 20 seconds.
+    [Lv 4]: Natural recovery every 18 seconds.
+    [Lv 5]: Natural recovery every 16 seconds.
+    [Lv 6]: Natural recovery every 14 seconds.
+    [Lv 7]: Natural recovery every 12 seconds.
+    [Lv 8]: Natural recovery every 10 seconds.
+    [Lv 9]: Natural recovery every 8 seconds.
+    [Lv 10]: Natural recovery every 6 seconds.`,
   },
 ];
 
