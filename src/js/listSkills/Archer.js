6bc88cfb6fb1125b7889d573d7051a6644bff30b
Makis <Nazgulius@yandex.ru>
2025-08-11 16:34:36 +0300

    Update Archer.js

--- src/js/listSkills/Archer.js
+++ src/js/listSkills/Archer.js
@@ -28,21 +28,28 @@ export const skillsArcher = [
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
+Weapon: Bow 
 Type: Physical
 Target: Enemy
-Range: Bow + Vulture's Eye Range
+Element: Ammunition
+After Cast Delay: A.Delay - 0.22s
+Cooldown: A.Delay
+Range: Weapon's range
+Hits: 2
 Requirement: None
-Description: Double Strafe unleashes a powerful arrow shot that deals double damage to the target. Consumes: 2x Arrow.
-[Lv 1]: Atk 110% x 2 Hits,
-[Lv 2]: Atk 120% x 2 Hits,
-[Lv 3]: Atk 130% x 2 Hits,
-[Lv 4]: Atk 140% x 2 Hits,
-[Lv 5]: Atk 150% x 2 Hits,
-[Lv 6]: Atk 160% x 2 Hits,
-[Lv 7]: Atk 170% x 2 Hits,
-[Lv 8]: Atk 180% x 2 Hits,
-[Lv 9]: Atk 190% x 2 Hits,
-[Lv 10]: Atk 200% x 2 Hits`,
+Description: Deals ranged P.DMG to the target.
+Catalyst: 1x Arrow
+[Lv. 1]: ATK 110% x Hits. SP Cost: 6 
+[Lv. 2]: ATK 120% x Hits. SP Cost: 6 
+[Lv. 3]: ATK 130% x Hits. SP Cost: 7 
+[Lv. 4]: ATK 140% x Hits. SP Cost: 7 
+[Lv. 5]: ATK 150% x Hits. SP Cost: 8
+[Lv. 6]: ATK 160% x Hits. SP Cost: 8 
+[Lv. 7]: ATK 170% x Hits. SP Cost: 9 
+[Lv. 8]: ATK 180% x Hits. SP Cost: 9 
+[Lv. 9]: ATK 190% x Hits. SP Cost: 10 
+[Lv.10]: ATK 200% x Hits. SP Cost: 10
+Formula: ATK (%): (100 + (10 x Skill Lv)) x Hits `,
     img: doubleStrafe,
   },
   {
@@ -59,24 +66,31 @@ Description: Double Strafe unleashes a powerful arrow shot that deals double dam
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
+Weapon: Bow/Instrument/Whip
 Type: Physical
 Target: Ground
+Element: Ammunition
+Variable Cast Time: 0.38s
+After Cast Delay: A.Delay - 0.22s 
+Cooldown: 6s
 Range: 12
+Hits: 4
 Requirement: Double Strafe Lv: 5
-Description: Unleashes a barrage of arrows, raining down on enemies within a 7x7 area and dealing damage in multiple waves. Each wave delivers four hits. Number of waves increases based on the user's ASPD. Consumes: 6x Arrow.
-[Lv 1]: Wave ATK 55%, Duration: 1.9 s,
-[Lv 2]: Wave ATK 60%, Duration: 2.1 s,
-[Lv 3]: Wave ATK 65%, Duration: 2.3 s,
-[Lv 4]: Wave ATK 70%, Duration: 2.5 s,
-[Lv 5]: Wave ATK 75%, Duration: 2.7 s,
-[Lv 6]: Wave ATK 80%, Duration: 2.9 s,
-[Lv 7]: Wave ATK 85%, Duration: 3.1 s,
-[Lv 8]: Wave ATK 90%, Duration: 3.3 s,
-[Lv 9]: Wave ATK 95%, Duration: 3.5 s,
-[Lv 10]: Wave ATK 100%, Duration: 3.7 s,
-New calculation: ATK% = 50 + (5 × Skill Lv) * (1 + (BaseLevel² / 10000)).
-Details: AfterCastActDelay set as ASPD + 220; CastTime between 70 and 700; AmmoAmount: 1.
-`,
+Description: Deals ranged P.DMG to enemies within a 7x7 AoE at the targeted location, hitting in waves every 0.2s.
+Damage scales with Base Level, and wave
+interval scales with A.Delay.
+Catalyst: 1x Arrow!
+[Lv. 1]: ATK 55% x Waves Duration: 2.00s. SP Cost: 7
+[Lv. 2]: ATK 60% x Waves Duration: 2.10s. SP Cost: 9 
+[Lv. 3]: ATK 65% x Waves Duration: 2.30s. SP Cost: 11 
+[Lv. 4]: ATK 70% x Waves Duration: 2.50s. SP Cost: 13 
+[Lv. 5]: ATK 75% x Waves Duration: 2.70s. SP Cost: 15 
+[Lv. 6]: ATK 80% x Waves Duration: 2.90s. SP Cost: 17 
+[Lv. 7]: ATK 85% x Waves Duration: 3.10s. SP Cost: 19 
+[Lv. 8]: ATK 90% x Waves Duration: 3.30s. SP Cost: 21 
+[Lv. 9]: ATK 95% x Waves Duration: 3.50s. SP Cost: 23 
+[Lv.10]: ATK 100% x Waves Duration: 3.70s. SP Cost: 25
+Formula: ATK (%): (50 + (5 × Skill Lv)) x (1 + ((Base Lv^2) / 10000)) x Waves `,
     img: arrowShower,
   },
   {
@@ -91,23 +105,27 @@ Details: AfterCastActDelay set as ASPD + 220; CastTime between 70 and 700; AmmoA
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
+Weapon: Bow/Instrument/Whip
 Type: Physical
 Target: Enemy
-Range: Bow + Vulture's Eye Range
+Element: Ammunition
+After Cast Delay: A.Delay 0.22s 
+Cooldown: A.Delay + 0.36s
+Range: Weapon's range
 Requirement: None
-Description: Draw their bowstring to its maximum tension, firing a powerful volley that not only deals damage but also knocksback the target 6 cells and reduces their Movement Speed by 15%. Consumes: 1x Arrow.
-Targets pushed by Charge Arrow cannot be pushed again for 4 seconds.
-[Lv 1]: Atk 140%,
-[Lv 2]: Atk 180%,
-[Lv 3]: Atk 220%,
-[Lv 4]: Atk 260%,
-[Lv 5]: Atk 300%,
-[Lv 6]: Atk 340%,
-[Lv 7]: Atk 380%,
-[Lv 8]: Atk 420%,
-[Lv 9]: Atk 460%,
-[Lv 10]: Atk 500%
-`,
+Description: Deals ranged P.DMG to the target, Knocking it back 6 cells and temporarily increasing its WD by 15%.
+Catalyst: 1x Arrow
+[Lv. 1]: ATK 140%, SP Cost: 6 WD Duration: 1.40s
+[Lv. 2]: ATK 180%, SP Cost: 7 WD Duration: 1.80s
+[Lv. 3]: ATK 220%, SP Cost: 8 WD Duration: 2.20s
+[Lv. 4]: ATK 260%, SP Cost: 9 WD Duration: 2.60s
+[Lv. 5]: ATK 300%, SP Cost: 10 WD Duration: 3.00s
+[Lv. 6]: ATK 340%, SP Cost: 11 WD Duration: 3.40s
+[Lv. 7]: ATK 380%, SP Cost: 12 WD Duration: 3.80s
+[Lv. 8]: ATK 420%, SP Cost: 13 WD Duration: 4.20s
+[Lv. 9]: ATK 460%, SP Cost: 14 WD Duration: 4.60s
+[Lv.10]: ATK 500%, SP Cost: 15 WD Duration: 5.00s
+Formula: ATK (%): 100 + (40 x Skill Lv) `,
     img: chargeArrow,
   },
   {
@@ -129,17 +147,17 @@ Targets pushed by Charge Arrow cannot be pushed again for 4 seconds.
 Skill Form: Passive
 Type: Misc
 Requirement: None
-Description: Sharpens the user focus, increasing their DEX and enhancing their Hit Rate.
-[Lv 1]: DEX +1, Hit Rate +1%,
-[Lv 2]: DEX +2, Hit Rate +2%,
-[Lv 3]: DEX +3, Hit Rate +3%,
-[Lv 4]: DEX +4, Hit Rate +4%,
-[Lv 5]: DEX +5, Hit Rate +5%,
-[Lv 6]: DEX +6, Hit Rate +6%,
-[Lv 7]: DEX +7, Hit Rate +7%,
-[Lv 8]: DEX +8, Hit Rate +8%,
-[Lv 9]: DEX +9, Hit Rate +9%,
-[Lv 10]: DEX +10, Hit Rate +10%`,
+Description: Increases DEX and ACC
+[Lv. 1]: DEX +1. ACC +1%
+[Lv. 2]: DEX +2. ACC +2%
+[Lv. 3]: DEX +3, ACC +3% 
+[Lv. 4]: DEX +4, ACC +4% 
+[Lv. 5]: DEX +5. ACC +5% 
+[Lv. 6]: DEX +6. ACC +6% 
+[Lv. 7]: DEX +7, ACC +7% 
+[Lv. 8]: DEX +8. ACC +8% 
+[Lv. 9]: DEX +9, ACC +9% 
+[Lv.10]: DEX +10. ACC +10%`,
     img: owlsEye,
   },
   {
@@ -157,18 +175,19 @@ Description: Sharpens the user focus, increasing their DEX and enhancing their H
     inform: `Max Lv: 10
 Skill Form: Passive
 Type: Misc
+Weapon: Bow/Instrument/Whip
 Requirement: Owl's Eye Lv: 3
-Description: Enhances Attack and Attack Range with Bow class weapons. The benefits of this skill are improved upon reaching 2nd class. When [Lv 10] increases Hit Rate in 6%.
-[Lv 1]: Atk +2, Range +2, 2nd Class Additional Atk +1,
-[Lv 2]: Atk +4, Range +2, 2nd Class Additional Atk +2,
-[Lv 3]: Atk +6, Range +3, 2nd Class Additional Atk +3,
-[Lv 4]: Atk +8, Range +3, 2nd Class Additional Atk +4,
-[Lv 5]: Atk +10, Range +4, 2nd Class Additional Atk +5,
-[Lv 6]: Atk +12, Range +4, 2nd Class Additional Atk +6,
-[Lv 7]: Atk +14, Range +5, 2nd Class Additional Atk +7,
-[Lv 8]: Atk +16, Range +5, 2nd Class Additional Atk +8,
-[Lv 9]: Atk +18, Range +6, 2nd Class Additional Atk +9,
-[Lv 10]: Atk +20, Range +6, 2nd Class Additional Atk +10`,
+Description: Increases E.ATK and Range while wielding a Bow, Instrument or Whip. Hunters, Bards and Gypsies gain extra E.ATK. At max level, also grants +6% ACC.
+[Lv. 1]: E.ATK +2. Range +2 
+[Lv. 2]: E.ATK +4. Range +2
+[Lv. 3]: E.ATK +6. Range +3 
+[Lv. 4]: E.ATK +8. Range +3 
+[Lv. 5]: E.ATK +10. Range +4 
+[Lv. 6]: E.ATK +12. Range +4 
+[Lv. 7]: E.ATK +14. Range +5 
+[Lv. 8]: E.ATK +16. Range +5 
+[Lv. 9]: E.ATK +18. Range +6 
+[Lv.10]: E.ATK +20. Range +6`,
     img: vulturesEye,
   },
   {
@@ -190,20 +209,23 @@ Description: Enhances Attack and Attack Range with Bow class weapons. The benefi
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
-Type: Physical
+Type: Supportive 
 Target: Self
+After Cast Delay: 0.30s
+Cooldown: A.Delay
 Requirement: Vulture's Eye Lv: 1
-Description: Boosts AGI and DEX for 240 seconds as well as detects nearby hidden characters in 3x3 AoE.
-[Lv 1]: DEX/AGI +3%,
-[Lv 2]: DEX/AGI +4%,
-[Lv 3]: DEX/AGI +5%,
-[Lv 4]: DEX/AGI +6%,
-[Lv 5]: DEX/AGI +7%,
-[Lv 6]: DEX/AGI +8%,
-[Lv 7]: DEX/AGI +9%,
-[Lv 8]: DEX/AGI +10%,
-[Lv 9]: DEX/AGI +11%,
-[Lv 10]: DEX/AGI +12%`,
+Description: Boosts DEX and AGI for 240s.
+Reveals Invisible enemies within a 3x3 AoE when used.
+[Lv. 1]: DEX/AGI +3%
+[Lv. 2]: DEX/AGI +4%
+[Lv. 3]: DEX/AGI +5%
+[Lv. 4]: DEX/AGI +6%
+[Lv. 5]: DEX/AGI +7% 
+[Lv. 6]: DEX/AGI +8% 
+[Lv. 7]: DEX/AGI +9% 
+[Lv. 8]: DEX/AGI +10% 
+[Lv. 9]: DEX/AGI +11% 
+[Lv.10]: DEX/AGI +12%`,
     img: improveConcentration,
   },
   {
@@ -218,15 +240,32 @@ Description: Boosts AGI and DEX for 240 seconds as well as detects nearby hidden
     maxLevel: 5,
     inform: `Max Lv: 5
 Skill Form: Active
-Type: Physical
+Type: Supportive 
 Target: Self
 Requirement: Owl's Eye Lv: 3
-Description: Uses various items to craft arrows. The quantity of arrows crafted receives an additional bonus based on Base Level, Job Level, and all attributes, with INT being the most influential. The relevance of attributes increases exponentially as they grow. The skill level affects the efficiency of the additional production. Regardless of the skill level, the additional production varies.
-[Lv 1]: No Additional Bonus,
-[Lv 2]: Additional Efficiency -75%,
-[Lv 3]: Additional Efficiency -50%,
-[Lv 4]: Additional Efficiency -25%,
-[Lv 5]: Full Efficiency`,
+Description: Crafts arrows from items.
+Base Level, Job Level, and Stats increase the number of crafted arrows.
+Guide: Flechery Creation Guide
+[Lv. 1]: Bonus Efficiency: 0%, SP Cost: 2 
+[Lv. 2]: Bonus Efficiency: 25%, SP Cost: 4 
+[Lv. 3]: Bonus Efficiency: 50%, SP Cost: 6 
+[Lv. 4]: Bonus Efficiency: 75%, SP Cost: 8 
+[Lv. 5]: Bonus Efficiency: 100%, SP Cost: 10
+Crafted Arrows :
+Base Amount + Bonus Amount
+Bonus Amount:
+((Base Amount x ((Level Bonus + Stats Bonus) x ((Skill Lv x 25) - 25))) / 100)
+
+Level Bonus: 
+((Base Lv x 100) / 200) + ((Job Lv x 100) / 140) / 100
+
+Stats Bonus:
+((STR^2 / 10) + (AGI^2 / 10) + (VIT^2 / 10) + (INT^2) + (DEX^2 / 10) + (LUK^2 / 10)) / 100
+
+Random Bonus Amount:
+10% chance for the bonus to be reduced to 25%
+70% chance for the bonus to be reduced to 50%
+ 20% chance for no reductions `,
     img: fletchery,
   },
   {
@@ -241,10 +280,12 @@ Description: Uses various items to craft arrows. The quantity of arrows crafted 
     maxLevel: 1,
     inform: `Max Lv: 1
 Skill Form: Active
-Type: Physical
+Type: Supportive 
 Target: Self
+SP Cost: 20
 Requirement: Fletchery Lv: 3
-Description: Allows the user to create a quiver using 500 arrows. The process requires an Empty Quiver as the base item for crafting.`,
+Description: Stores arrows inside a Quiver. Each Quiver stores 500 arrows of the same type.
+Catalyst: 1x Empty Quiver `,
     img: quivery,
   },
 ];
