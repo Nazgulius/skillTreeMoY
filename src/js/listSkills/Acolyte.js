04c97550adcf9666fef89a21433b58ee1f4c45db
Makis <Nazgulius@yandex.ru>
2025-08-15 13:41:49 +0300

    Update Acolyte.js

--- src/js/listSkills/Acolyte.js
+++ src/js/listSkills/Acolyte.js
@@ -43,17 +43,18 @@ export const skillsAcolyte = [
 Skill Form: Passive
 Type: Physical
 Requirement: None
-Description: Passively raises Soft Defense and Soft Magic Defense against Demon and Undead race monsters. Base level affects damage reduction. When receiving damage from Demon and Undead race monsters, there is a chance to activate the Divine Protection effect for 10 seconds. This effect reduces the next damage you receive from Demon and Undead race monsters, with the reduction being equal to half of your base level in %.
-[Lv 1]: Reduction: 3 + Base Level, Chance of effect: 1%,
-[Lv 2]: Reduction: 6 + Base Level, Chance of effect: 2%,
-[Lv 3]: Reduction: 9 + Base Level, Chance of effect: 3%,
-[Lv 4]: Reduction: 12 + Base Level, Chance of effect: 4%,
-[Lv 5]: Reduction: 15 + Base Level, Chance of effect: 5%,
-[Lv 6]: Reduction: 18 + Base Level, Chance of effect: 6%,
-[Lv 7]: Reduction: 21 + Base Level, Chance of effect: 7%,
-[Lv 8]: Reduction: 24 + Base Level, Chance of effect: 8%,
-[Lv 9]: Reduction: 27 + Base Level, Chance of effect: 9%,
-[Lv 10]: Reduction: 30 + Base Level, Chance of effect: 10%`,
+Description: Grants S.DEF and S.MDEF against Demon, Undead and Corrupt monsters, scaling with Base Level. Also grants a chance. to activate Divine Protection for 10s when receiving damage from these monsters. 
+[Lv. 1]; S.DEF/S.MDEF +3. Chance: 1% 
+[Lv. 2]: S.DEF/S.MDEF +6. Chance: 2% 
+[Lv. 3]: S.DEF/S.MDEF +9. Chance: 3% 
+[Lv. 4]: S.DEF/S.MDEF +12. Chance: 4% 
+[Lv. 5]: S.DEF/S.MDEF +15. Chance: 5% 
+[Lv. 6]: S.DEF/S.MDEF +18. Chance: 6% 
+[Lv. 7]: S.DEF/S.MDEF +21. Chance: 7% 
+[Lv. 8]: S.DEF/S.MDEF +24. Chance: 8% 
+[Lv. 9]: S.DEF/S.MDEF +27. Chance: 9% 
+[Lv.10]: S.DEF/S.MDEF +30. Chance: 10%
+Formula: S.DEF/S.MDEF: (Skill Lv x 3) + Base Lv `,
     img: divineProtection,    
   }, 
   {
@@ -73,17 +74,17 @@ Description: Passively raises Soft Defense and Soft Magic Defense against Demon 
 Skill Form: Passive
 Type: Physical
 Requirement: Divine Protection Lv: 3
-Description: Passively raises damage against Demon and Undead race monsters. Base level affects damage bonus. Damage increased by this skill is added after final damage is calculated. When attacking Demon and Undead race monsters, there is a chance to activate the Demon Bane effect for 10 seconds. This effect increases the damage of your next attack or skill in Demon and Undead race monsters, with the increased damage being equal to half of your base level in %.
-[Lv 1]: Damage: 3 + Base Level, Chance of effect: 1%,
-[Lv 2]: Damage: 6 + Base Level, Chance of effect: 2%,
-[Lv 3]: Damage: 9 + Base Level, Chance of effect: 3%,
-[Lv 4]: Damage: 12 + Base Level, Chance of effect: 4%,
-[Lv 5]: Damage: 15 + Base Level, Chance of effect: 5%,
-[Lv 6]: Damage: 18 + Base Level, Chance of effect: 6%,
-[Lv 7]: Damage: 21 + Base Level, Chance of effect: 7%,
-[Lv 8]: Damage: 24 + Base Level, Chance of effect: 8%,
-[Lv 9]: Damage: 27 + Base Level, Chance of effect: 9%,
-[Lv 10]: Damage: 30 + Base Level, Chance of effect: 10%`,
+Description: Grants Mastery ATK and Mastery MATK against Demon, Undead and Corrupt monsters, scaling with Base Level. Also grants a chance to activate Demon Bane for 10s when deals damage to these monsters.
+[Lv. 1]: Mastery +3. Chance: 1% 
+[Lv. 2]: Mastery +6. Chance: 2% 
+[Lv. 3]: Mastery +9. Chance: 3% 
+[Lv. 4]: Mastery +12. Chance: 4% 
+[Lv. 5]: Mastery +15. Chance: 5% 
+[Lv. 6]: Mastery +18. Chance: 6% 
+[Lv. 7]: Mastery +21. Chance: 7% 
+[Lv. 8]: Mastery +24. Chance: 8% 
+[Lv. 9]: Mastery +27. Chance: 9% 
+[Lv.10]: Mastery +30. Chance: 10%`,
     img: demonBane, 
   },
   {
@@ -104,20 +105,24 @@ Description: Passively raises damage against Demon and Undead race monsters. Bas
     inform: `Max Lv: 10
 Skill Form: Active
 Type: Magical
+Type: Supportive
 Target: Ally
+After Cast Delay: 1s
 Range: 9
 Requirement: None
-Description: Restores HP of a single target. This skill is affected by Base Level, INT and MAtk. Versus Corrupt property targets, inflicts Holy property damage equal 75% the amount of the HP restored.
-[Lv 1]: SP Cost: 13,
-[Lv 2]: SP Cost: 16,
-[Lv 3]: SP Cost: 19,
-[Lv 4]: SP Cost: 22,
-[Lv 5]: SP Cost: 25,
-[Lv 6]: SP Cost: 28,
-[Lv 7]: SP Cost: 31,
-[Lv 8]: SP Cost: 34,
-[Lv 9]: SP Cost: 37,
-[Lv 10]: SP Cost: 40`,
+Description: Restores the target's HP, scaling with MATK and the Weapon's Level.
+Against Undead and Corrupt monsters, deals Holy property M.DMG damage equal to 75% of the amount of the HP restored. 
+[Lv. 1]: SP Cost: 13
+[Lv. 2]: SP Cost: 16
+[Lv. 3]: SP Cost: 19
+[Lv. 4]: SP Cost: 22
+[Lv. 5]: SP Cost: 25
+[Lv. 6]: SP Cost: 28
+[Lv. 7]: SP Cost: 31
+[Lv. 8]: SP Cost: 34 
+[Lv. 9]: SP Cost: 37 
+[Lv.10]: SP Cost: 40
+Formula: Heal: MATK + (((Skill Lv x 100) / 4) x Weapon Lv) `,
     img: heal, 
   },
   {
@@ -130,16 +135,18 @@ Description: Restores HP of a single target. This skill is affected by Base Leve
     maxLevel: 5,
     inform: `Max Lv: 1 (or 5)
 Skill Form: Active
-Type: Magical
+Type: Supportive
 Target: Ally
+Cooldown: 35
 Range: 9
 Requirement: Heal Lv: 2
-Description: Attempts to cleanse a single target from the Silence, Chaos and Blind status. If successful, grants a temporary 20% increase in resistance to the cured status. Caster INT and Base Level, target INT or LUK and Base Level increases chance of cleanse. This skill is initially learned at Level 1, with higher levels unlocked through special items.
-[Lv 1]: Resistance duration: 10 seconds. Cooldown: 3 seconds,
-[Lv 2]: Resistance duration: 20 seconds. Cooldown: 2.5 seconds,
-[Lv 3]: Resistance duration: 30 seconds. Cooldown: 2 seconds,
-[Lv 4]: Resistance duration: 40 seconds. Cooldown: 1.5 seconds,
-[Lv 5]: Resistance duration: 50 seconds. Cooldown: 1 seconds`,
+Description: Attempts to remove Silence, Chaos and Blind from the target.
+The chance scales with the user's INT and Base Level, and target's INT, LUK and Base Level.
+Also grants +20% resistance to the removed statuses for 10s in a successful attempt.
+
+Silence Chance (%): ((Base Lv x 10) + (INT x 40) + (Target Base Lv x 10) + (Target INT x 40)) / 100 
+Chaos Chance (%): ((Base Lv x 10) + (INT x 40)) + (Target Base Lv x 10) + (Target LUK × 40)) / 100
+Blind Chance (%): ((Base Lv x 10) + (INT x 40) + (Target Base Lv x 10) + (Target INT x 20) + (Target LUK x 20)) / 100 `,
     img: cure, 
   },
   {
@@ -155,20 +162,28 @@ Description: Attempts to cleanse a single target from the Silence, Chaos and Bli
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
-Type: Magical
-Target: Self
+Type: Supportive 
+Target: Ally
+Variable Cast Time: 0.70s
+Fixed Cast Time: 0.30s
+After Cast Delay: 1s 
+Cooldown: A.Delay 
+Range: 9
 Requirement: Divine Protection Lv: 3
-Description: Increases VIT, Soft Defense and MaxHP on the user and all party members in a 14x14 area around the user. Additionally, the bonus of VIT increases by +1 for every 14 Job Levels of the user. Duration of this ability is 240 seconds.
-[Lv 1]: VIT +1, Soft Def +5%, HP +50,
-[Lv 2]: VIT +2, Soft Def +10%, HP +100,
-[Lv 3]: VIT +3, Soft Def +15%, HP +150,
-[Lv 4]: VIT +4, Soft Def +20%, HP +200,
-[Lv 5]: VIT +5, Soft Def +25%, HP +250,
-[Lv 6]: VIT +6, Soft Def +30%, HP +300,
-[Lv 7]: VIT +7, Soft Def +35%, HP +350,
-[Lv 8]: VIT +8, Soft Def +40%, HP +400,
-[Lv 9]: VIT +9, Soft Def +45%, HP +450, 
-[Lv 10]: VIT +10, Soft Def +50%, HP +500`,
+Description: Increases VIT, S.DEF and Max HP of the target for 240s.
+The VIT bonus scales with Job Level, and the Max HP bonus scales with the target's Base Level.
+[Lv. 1]; VIT +1. S.DEF +7%, SP Cost: 22 
+[Lv. 2]: VIT +2. S.DEF +9%, SP Cost: 24 
+[Lv. 3]: VIT +3. S.DEF +11%, SP Cost: 26 
+[Lv. 4]: VIT +4. S.DEF +13%, SP Cost: 28 
+[Lv. 5]: VIT +5. S.DEF +15%, SP Cost: 30
+[Lv. 6]; VIT +6. S.DEF +17%, SP Cost: 32 
+[Lv. 7]: VIT +7. S.DEF +19%, SP Cost: 34 
+[Lv. 8]: VIT +8. S.DEF +21%, SP Cost: 36 
+[Lv. 9]: VIT +9. S.DEF +23%, SP Cost: 38 
+[Lv. 10]: VIT +10. S.DEF +25%, SP Cost: 40
+Formula: VIT Bonus: Skill Lv + (Job Lv / 14) 
+MaxHP Bonus: (Skill Lv + 5) x Target Base Lv `,
     img: angelus, 
   },
   {
@@ -181,21 +196,27 @@ Description: Increases VIT, Soft Defense and MaxHP on the user and all party mem
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
-Type: Magical
+Type: Supportive
 Target: Ally
 Range: 9
 Requirement: Divine Protection Lv: 5
-Description: Temporarily increases STR, INT, DEX and reduces Variable Cast Time of the specified character. Additionally, the bonus of STR, INT, and DEX increases by +1 for every 14 job levels of the user. Also has a chance to cure a target of Curse status. When used on Undead and Demon monsters Blessing will reduce their STR, INT and DEX by 50%. When the user becomes a High Priest, Blessing becomes a 5x5 area effect around the target, but the SP cost increases by 2 for each affected target. Duration of this ability is 240 seconds.
-[Lv 1]: STR, INT, DEX +1, Variable Cast Time -1%,
-[Lv 2]: STR, INT, DEX +2, Variable Cast Time -2%,
-[Lv 3]: STR, INT, DEX +3, Variable Cast Time -3%,
-[Lv 4]: STR, INT, DEX +4, Variable Cast Time -4%,
-[Lv 5]: STR, INT, DEX +5, Variable Cast Time -5%,
-[Lv 6]: STR, INT, DEX +6, Variable Cast Time -6%,
-[Lv 7]: STR, INT, DEX +7, Variable Cast Time -7%,
-[Lv 8]: STR, INT, DEX +8, Variable Cast Time -8%,
-[Lv 9]: STR, INT, DEX +9, Variable Cast Time -9%,
-[Lv 10]: STR, INT, DEX +10, Variable Cast Time -10%`,
+Description: Increases STR, INT and DEX of the target for 240s, scaling with Job Level.
+Also reduces its skill's VCT.
+Has a chance to remove Curse, which scales with the user's INT and Base Level, and the target's LUK and Base Level.
+Deals M.DMG against Demon, Undead and Corrupt monsters. Also decreases their STR, INT and DEX by 50% for 240s.
+[Lv. 1]: Stats +1. VCT -1%, SP Cost: 22 
+[Lv. 2]: Stats +2. VCT -2%, SP Cost: 24 
+[Lv. 3]: Stats +3. VCT -3%, SP Cost: 26 
+[Lv. 4]: Stats +4. VCT -4%, SP Cost: 28 
+[Lv. 5]: Stats +5, VCT -5%, SP Cost: 30
+[Lv. 6]: Stats +6. VCT -6%, SP Cost: 32 
+[Lv. 7]: Stats +7. VCT -7%, SP Cost: 34 
+[Lv. 8]: Stats +8. VCT -8%, SP Cost: 36 
+[Lv. 9]: Stats +9. VCT -9%, SP Cost: 38 
+[Lv. 10]: Stats +10, VCT -10%, SP Cost: 40
+Formula: Stats Bonus: Skill Lv + (Job Lv / 14)
+MATK (%): 50
+Chance (%); ((Base Lv x 10) + (INT x 40)) + (Target Base Lv x 10) + (Target LUK x 40)) / 100 `,
     img: blessing, 
   },
   {
@@ -208,21 +229,25 @@ Description: Temporarily increases STR, INT, DEX and reduces Variable Cast Time 
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
-Type: Magical
+Type: Supportive
 Target: Ally
-Range: 9
+Variable Cast Time: 1.40s
+Fixed Cast Time: 0.60s
+After Cast Delay: 1s
+Cooldown: Cooldown
 Requirement: Heal Lv: 3
-Description: Increases targeted characters AGI, Movement and Attack Speed for the duration of skill. Additionally, the bonus of AGI increases by +1 for every 14 job levels of the user. Drains 15 HP from the caster after each cast. When used on target that affected by Decrease Agility status, this skill will release target from that. When the user becomes a High Priest, Increase agility becomes a 5x5 area effect around the target, but the SP cost increases by 2 for each affected target. Duration of this ability is 240 seconds.
-[Lv 1]: AGI +1, Atk Speed: +1%,
-[Lv 2]: AGI +2, Atk Speed: +2%,
-[Lv 3]: AGI +3, Atk Speed: +3%,
-[Lv 4]: AGI +4, Atk Speed: +4%,
-[Lv 5]: AGI +5, Atk Speed: +5%,
-[Lv 6]: AGI +6, Atk Speed: +6%,
-[Lv 7]: AGI +7, Atk Speed: +7%,
-[Lv 8]: AGI +8, Atk Speed: +8%,
-[Lv 9]: AGI +9, Atk Speed: +9%,
-[Lv 10]: AGI +10, Atk Speed: +10%`,
+Description: Reduces DAA and increases AGI of the target. Also reduces its WD by 18%, The AGI bonus scales with Job Level.
+[Lv. 1]: AGI +1. DAA -1%, SP Cost: 22 
+[Lv. 2]: AGI +2. DAA -2%, SP Cost: 24 
+[Lv. 3]: AGI +3. DAA -3%, SP Cost: 26 
+[Lv. 4]: AGI +4. DAA -4%, SP Cost: 28 
+[Lv. 5]: AGI +5. DAA -5%, SP Cost: 30 
+[Lv. 6]: AGI +6. DAA -6%, SP Cost: 32 
+[Lv. 7]: AGI +7. DAA -7%, SP Cost: 34 
+[Lv. 8]: AGI +8. DAA -8%, SP Cost: 36 
+[Lv. 9]: AGI +9. DAA -9%, SP Cost: 38 
+[Lv.10]: AGI +10. DAA -10%, SP Cost: 40
+Formula: AGI Bonus: Skill Lv + (Job Lv / 14) `,
     img: increaseAgility, 
   },  
   {
@@ -237,19 +262,24 @@ Description: Increases targeted characters AGI, Movement and Attack Speed for th
 Skill Form: Active
 Type: Magical
 Target: Enemy
+Variable Cast Time: 0.70s
+Fixed Cast Time: 0.30s
+After Cast Delay: A.Delay 0.28s Cooldown: A.Delay
 Range: 9
 Requirement: Increase Agility Lv: 1
-Description: Decreases targeted characters AGI, Movement and Attack Speed for the duration of skill. Additionally, the reduction of AGI increases by 1 for every 14 job levels of the user. When the user becomes a High Priest, Decrease Agility becomes a 3x3 area effect around the target, but the SP cost increases by 4 for each affected target.
-[Lv 1]: AGI -1, Movement Speed: -1%, Duration: 15 Seconds,
-[Lv 2]: AGI -2, Movement Speed: -2%, Duration: 20 Seconds,
-[Lv 3]: AGI -3, Movement Speed: -3%, Duration: 25 Seconds,
-[Lv 4]: AGI -4, Movement Speed: -4%, Duration: 30 Seconds,
-[Lv 5]: AGI -5, Movement Speed: -5%, Duration: 35 Seconds,
-[Lv 6]: AGI -6, Movement Speed: -6%, Duration: 40 Seconds,
-[Lv 7]: AGI -7, Movement Speed: -7%, Duration: 45 Seconds,
-[Lv 8]: AGI -8, Movement Speed: -8%, Duration: 50 Seconds,
-[Lv 9]: AGI -9, Movement Speed: -9%, Duration: 55 Seconds,
-[Lv 10]: AGI -10, Movement Speed: -10%, Duration: 60 Seconds`,
+Description: Reduces AGI of the target. Also increases its WD by 10%. 
+The AGI reduction scales with Job Level. 
+[Lv. 1]: AGI -1. Duration: 15s. SP Cost: 22 
+[Lv. 2]: AGI -2. Duration: 20s. SP Cost: 24 
+[Lv. 3]: AGI -3. Duration: 25s. SP Cost: 26 
+[Lv. 4]: AGI -4. Duration: 30s. SP Cost: 28 
+[Lv. 5]: AGI -5. Duration: 35s. SP Cost: 30 
+[Lv. 6]: AGI -6. Duration: 40s. SP Cost: 32 
+[Lv. 7]: AGI -7. Duration: 45s. SP Cost: 34 
+[Lv. 8]: AGI -8. Duration: 50s. SP Cost: 36 
+[Lv. 9]: AGI -9. Duration: 55s. SP Cost: 38 
+[Lv.10]: AGI -10. Duration: 60s. SP Cost: 40
+Formula: AGI Decrease: Skill Lv + (Job Lv / 14) `,
     img: decreaseAgility, 
   },
   {
@@ -265,21 +295,25 @@ Description: Decreases targeted characters AGI, Movement and Attack Speed for th
     maxLevel: 10,
     inform: `Max Lv: 10
 Skill Form: Active
-Type: Magical
+Type: Physical or Magical
 Target: Enemy
-Range: 9
+Element: Holy
+After Cast Delay: A.Delay - 0.465
+Cooldown: A.Delay
+Hits: 2
 Requirement: None
-Description: Summon holy light to counter evil. When used with Staffs or Books, it deals magical damage, has a casting time, and a spell range of 9 cells. When used with Maces or Knuckles, it becomes a physical attack, has no casting time, and becomes melee-ranged.
-[Lv 1]: ATK or MATK 135%,
-[Lv 2]: ATK or MATK 170%,
-[Lv 3]: ATK or MATK 205%,
-[Lv 4]: ATK or MATK 240%,
-[Lv 5]: ATK or MATK 275%,
-[Lv 6]: ATK or MATK 310%,
-[Lv 7]: ATK or MATK 345%,
-[Lv 8]: ATK or MATK 380%,
-[Lv 9]: ATK or MATK 415%,
-[Lv 10]: ATK or MATK 450%`,
+Description: Deals P.DMG or M.DMG to the target based on Weapon type.
+Enemies within a 3x3 AoE around the target receive half of the damage.
+[Lv. 1]: ATK/MATK 135%, SP Cost: 11 VCT: 0.10s. FCT: 0.04s
+[Lv. 2]: ATK/MATK 170%, SP Cost: 12 VCT: 0.20s. FCT: 0.08s
+[Lv. 3]: ATK/MATK 205%, SP Cost: 13 VCT: 0.30s. FCT: 0.125
+[Lv. 4]: ATK/MATK 240%, SP Cost: 14 VCT: 0.40s. FCT: 0.16s
+[Lv. 5]: ATK/MATK 275%, SP Cost: 15 VCT: 0.50s. FCT: 0.20s
+[Lv. 6]: ATK/MATK 310%, SP Cost: 16 VCT: 0.60s. FCT: 0.24s
+[Lv. 7]: ATK/MATK 345%, SP Cost: 17 VCT: 0.70s. FCT: 0.28s
+[Lv. 8]: ATK/MATK 380%, SP Cost: 18 VCT: 0.80s. FCT: 0.32s
+[Lv. 9]: ATK/MATK 415%, SP Cost: 19 VCT: 0.90s. FCT: 0.36s
+[Lv.10]: ATK/MATK 450%, SP Cost: 20 VCT: 1.00s. FCT: 0.40s`,
     img: holyLight, 
   },
   {
@@ -301,10 +335,13 @@ Description: Summon holy light to counter evil. When used with Staffs or Books, 
     maxLevel: 1,
     inform: `Max Lv: 1
 Skill Form: Active
-Type: Magical
+Type: Supportive 
 Target: Self
-Requirement: None
-Description: Uses 10 SP to summon Holy Spirit that will detect hidden characters in 5x5 cells area around the caster for 10 sec.`,
+After Cast Delay: A.Delay - 0.285 
+Cooldown: A.Delay
+SP Cost: 10
+Requirement: None 
+Description: Reveals Invisible enemies within a 7x7 AoE for 10s.`,
     img: ruwach, 
   },
   {
@@ -317,12 +354,15 @@ Description: Uses 10 SP to summon Holy Spirit that will detect hidden characters
     maxLevel: 2,
     inform: `Max Lv: 2
 Skill Form: Active
-Type: Magical
+Type: Supportive 
 Target: Self
+After Cast Delay: 1s
+Cooldown: 3s
 Requirement: Ruwach Lv: 1
-Description: Teleports caster to other area. Cant be used in an area that protected by Land Protector.
-[Lv 1]: Random Spot, SP Consumption: 10,
-[Lv 2]: Save Point, SP Consumption: 9`,
+Description: Warps the user to a different location instantly.
+Cannot be used while standing on a Land Protector cell.
+[Lv. 1]: Random Spot. SP Cost: 10 
+[Lv. 2]: Save Point. SP Cost: 9`,
     img: teleport, 
   },
   {
@@ -335,14 +375,19 @@ Description: Teleports caster to other area. Cant be used in an area that protec
     maxLevel: 3,
     inform: `Max Lv: 3
 Skill Form: Active
-Type: Magical
+Type: Supportive 
 Target: Ground
+After Cast Delay: 1s 
+Fixed Cast Time: 1s
+Max Instances: 3 
 Range: 9
 Requirement: Teleport Lv: 2
-Description: Creates a Warp Portal that will transport those that enter to the portal's destination. Up to 8 players can be transported regardless of the skill level. This skill is disabled within land protector effect. Catalyst: 1x Blue Gemstone.
-[Lv 1]: 1 custom position available,
-[Lv 2]: 2 custom position available,
-[Lv 3]: 3 custom position available`,
+Description: Opens a list of Memo Points. Creates a portal at the targeted location to the selected memo point. Up to 8 players can cross the portal.
+Older instances are removed to create new ones when reaching the instance limit.
+Catalyst: 1x Blue Gemstone
+[Lv. 1]: Memo Points: 1. SP Cost: 35 Duration: 10s
+[Lv. 2]: Memo Points: 2. SP Cost: 32 Duration: 15s
+[Lv. 3]: Memo Points: 3. SP Cost: 29 Duration: 20s`,
     img: warpPortal, 
   },
   {
@@ -357,11 +402,20 @@ Description: Creates a Warp Portal that will transport those that enter to the p
     maxLevel: 1,
     inform: `Max Lv: 1
 Skill Form: Active
-Type: Magical
+Type: Supportive 
 Target: Ground
+After Cast Delay: 0.50s 
+Cooldown: 0.50s
+Max Instances: 3 
 Range: 9
+SP Cost: 10
 Requirement: Warp Portal Lv: 3
-Description: Uses 10 SP to create, barrier in 3x3 cells around targeted, cell that for 10 sec will protect, anyone inside it from long range, physical attacks.`,
+Description: Creates a pillar at the targeted location, blocking all ranged P.DMG until its durability runs out or it reaches its 10-hit limit.
+The blocked damage is transferred to the pillar, reducing its durability.
+The durability scales with INT, Max SP and Base Level.
+Older instances are removed to create new ones when reaching the instance limit.
+
+Durability: 300+ (65 x (INT + Base Lv)) + MaxSP `,
     img: pneuma, 
   },
 ];
