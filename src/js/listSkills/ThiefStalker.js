7ff2beb40e72038b6813fe45c305de22107d0676
Makis <Nazgulius@yandex.ru>
2025-08-16 19:02:31 +0300

    Update ThiefStalker.js

--- src/js/listSkills/ThiefStalker.js
+++ src/js/listSkills/ThiefStalker.js
@@ -23,15 +23,18 @@ export const skillsStalker = [
     maxLevel: 5,
     inform: `Max Lv: 5
 Skill Form: Active
-Type: Physical
+Weapon: Dagger/Sword 
+Type: Supportive
 Target: Self
+After Cast Delay: A.Delay
 Requirement: One-Handed Sword Mastery Lv: 3
-Description: Deflects incoming attacks, reducing damage by half. If the attacker is using a Sword or Dagger, the reduced damage is reflected back to them. On monsters, reflects without weapon restriction. Cooldown of this skill decreases with each blocked attack. Only usable with daggers or swords.
-[Lv 1]: Parry Chance: 90%, Duration: 5 seconds, Cooldown 15 seconds,
-[Lv 2]: Parry Chance: 80%, Duration: 6 seconds, Cooldown 16 seconds,
-[Lv 3]: Parry Chance: 70%, Duration: 8 seconds, Cooldown 18 seconds,
-[Lv 4]: Parry Chance: 60%, Duration: 11 seconds, Cooldown 21 seconds,
-[Lv 5]: Parry Chance: 50%, Duration: 15 seconds, Cooldown 25 seconds`,
+Description: Deflects incoming attacks from monsters and from players wielding Daggers or Swords, reducing damage by half and reflecting it back to them.
+CD scales with skill level, and each deflected attack reduces CD.
+[Lv. 1]: Chance: 90%, CD Reduce: 1s Duration: 5s. Cooldown 15s. SP Cost: 10 
+[Lv. 2]: Chance: 80%, CD Reduce: 2s Duration: 6s. Cooldown 16s. SP Cost: 15 
+[Lv. 3]: Chance: 70%, CD Reduce: 3s Duration: 8s. Cooldown 18s. SP Cost: 20 
+[Lv. 4]: Chance: 60%, CD Reduce: 4s Duration: 11s. Cooldown 21s. SP Cost: 25 
+[Lv. 5]: Chance: 50%, CD Reduce: 5s Duration: 15s. Cooldown 25s. SP Cost: 30`,
     img: counterInstinct,
   },
   {
@@ -49,11 +52,17 @@ Description: Deflects incoming attacks, reducing damage by half. If the attacker
     maxLevel: 1,
     inform: `Max Lv: 1
 Skill Form: Active
-Type: Physical
+Type: Supportive
 Target: Enemy
+After Cast Delay: A.Delay + 0.50s
+Cooldown: A.Delay 
 Range: 1
+SP Cost: 30
 Requirement: Helm Stripping Lv: 2, Shield Stripping Lv: 2, Armor Stripping Lv: 2, Weapon Stripping Lv: 2
-Description: Attempts to forcibly remove the equipped weapon, shield, armor and headgear of a single target, temporarily preventing them from equipping it again. Does not work against monsters. The success rate is influenced by the difference between the users and the targets Base Level. Each level of the target reduces the duration by 0.5%. Success Chance: 10% + 1% for every level learned in Divest Helm, Divest Shield, Divest Armor, and Divest Weapon, Duration: 15 seconds`,
+Description: Attempts to forcibly Remove a single target's weapon, shield, armor, and headgear for 15s.
+Success rate scales with the learned level of Stripping Skills and the Base Level difference between the caster and target.
+Duration scales with the target's Base Level. Does not work against monsters.
+Formula: Chance (%): (100 + ((Helm Stripping Lv + Shield Stripping Lv + Armor Stripping Lv + Weapon Stripping Lv) x 10) + ((Base Lv - Target Lv) x 10)) / 10 `,
     img: fullStrip,
   },
   {
@@ -67,22 +76,28 @@ Description: Attempts to forcibly remove the equipped weapon, shield, armor and 
     skillName: "Simulation",
     maxLevel: 10,
     inform: `Max Lv: 10
-Skill Form: Active
-Type: Misc
+Skill Form: Toggle 
+Type: Supportive 
 Target: Self
+After Cast Delay: 0.30s
+Fixed Cast Time: 1s 
+Cooldown: A.Delay
+SP Cost: 80
 Requirement: Plagiarism Lv: 10
-Description: Adds +5 True ATK and MATK per level learned. Each basic attack has a chance to automatically cast a skill stolen by Plagiarism on the target. The chance and level of the auto-cast skill increase with the level of Simulation. Auto-casting skills have increased SP consumption, reducible by learned level of Simulation.
-[Lv 1]: Auto-Cast Level: 3, Auto-Cast Chance: 24%, Auto-Cast SP Cost: +45%,
-[Lv 2]: Auto-Cast Level: 3, Auto-Cast Chance: 23%, Auto-Cast SP Cost: +40%,
-[Lv 3]: Auto-Cast Level: 4, Auto-Cast Chance: 22%, Auto-Cast SP Cost: +35%,
-[Lv 4]: Auto-Cast Level: 4, Auto-Cast Chance: 21%, Auto-Cast SP Cost: +30%,
-[Lv 5]: Auto-Cast Level: 5, Auto-Cast Chance: 20%, Auto-Cast SP Cost: +25%,
-[Lv 6]: Auto-Cast Level: 5, Auto-Cast Chance: 19%, Auto-Cast SP Cost: +20%,
-[Lv 7]: Auto-Cast Level: 6, Auto-Cast Chance: 18%, Auto-Cast SP Cost: +15%,
-[Lv 8]: Auto-Cast Level: 6, Auto-Cast Chance: 17%, Auto-Cast SP Cost: +10%,
-[Lv 9]: Auto-Cast Level: 7, Auto-Cast Chance: 16%, Auto-Cast SP Cost: +5%,
-[Lv 10]: Auto-Cast Level: 7, Auto-Cast Chance: 15%, Auto-Cast SP Cost: +0%
-Now, Simulation opens up a list of possible skills to be autocast, including the user’s own skills.
+Description: Allows the caster to choose a skill from their list of eligible skills, granting a chance to auto-cast the chosen skill on basic attacks.
+The auto-cast consumes one-third of the chosen skill's SP Cost and cannot activate if there is not enough SP.
+The Auto-cast Level scales with the skill level. Passively increases T.DMG.
+[Lv. 1]: Auto-cast Level: 50% Auto-cast Chance: 30%, T.DMG +5
+[Lv. 2]: Auto-cast Level: 60% Auto-cast Chance: 26%, T.DMG +10 
+[Lv. 3]: Auto-cast Level: 60% Auto-cast Chance: 28%, T.DMG +15 
+[Lv. 4]: Auto-cast Level: 70% Auto-cast Chance: 22%, T.DMG +20 
+[Lv. 5]: Auto-cast Level: 70% Auto-cast Chance: 24%, T.DMG +25 
+[Lv. 6]: Auto-cast Level: 80% Auto-cast Chance: 18%, T.DMG +30 
+[Lv. 7]: Auto-cast Level: 80% Auto-cast Chance: 20%, T.DMG +35 
+[Lv. 8]: Auto-cast Level: 90% Auto-cast Chance: 16%, T.DMG +40 
+[Lv. 9]: Auto-cast Level: 90% Auto-cast Chance: 17%, T.DMG +45 
+[Lv.10]: Auto-cast Level: 100% Auto-cast Chance: 15%, T.DMG +50
+
 `,
     img: simulation,
   },
@@ -98,16 +113,19 @@ Now, Simulation opens up a list of possible skills to be autocast, including the
     skillName: "Stealth",
     maxLevel: 5,
     inform: `Max Lv: 5
-Skill Form: Active
-Type: Misc
+Skill Form: Toggle
+Type: Supportive 
 Target: Self
+SP Cost: 10
 Requirement: Hiding Lv: 5, Tunnel Drive Lv: 3
-Description: Enter a special Hiding status in which caster can move without being detected by all detecting skills. However, the caster will leave footprints and can be damaged by skills that target the ground. Ineffective against Insect, Devil and Boss monsters. After being hidden, gain STR, DEX and INT for 180 seconds.
-[Lv 1]: Stats Bonus: 2%, 60% Move Speed, SP cost: 12 every 10 seconds,
-[Lv 2]: Stats Bonus: 4%, 70% Move Speed, SP cost: 14 every 10 seconds,
-[Lv 3]: Stats Bonus: 6%, 80% Move Speed, SP cost: 16 every 10 seconds,
-[Lv 4]: Stats Bonus: 8%, 90% Move Speed, SP cost: 18 every 10 seconds,
-[Lv 5]: Stats Bonus: 10%, 100% Move Speed, SP cost: 20 every 10 seconds`,
+Description: Becomes Invisible.
+Cannot be revealed by any detection skills, and activates Stealth status for 180s. While invisible, drains SP every 10s, increases WD and leaves shoe prints behind while walking.
+Cannot attack in this mode and becomes visible if SP runs out.
+[Lv 1]: WD: +40%, SP Drain: 12 
+[Lv 2]: WD: +30%, SP Drain: 14
+[Lv 3]: WD: +20%, SP Drain: 16 
+[Lv 4]: WD: +10%, SP Drain: 18 
+[Lv 5]: WD: +0%, SP Drain: 20`,
     img: stealth,
   },
   {
@@ -124,19 +142,26 @@ Description: Enter a special Hiding status in which caster can move without bein
 Skill Form: Active
 Type: Physical
 Target: Enemy
-Range: 1
+After Cast Delay: A.Delay 0.09s
+Fixed Cast Time: 0.60s
+Cooldown: 1s
+Range: 1 + Weapon's range
+Hits: 3
 Requirement: Double Strafe Lv: 10
-Description: Shoots three arrows quickly at the same target. Learning this skill allows Double Strafe to accumulate Strafe Points, up to 3 max. Spending Strafe Points in Triple Strafe reduces its cooldown by 2 second per point and boosts damage based on the users AGI.
-[Lv 1]: Atk 216% x 3 Hits,
-[Lv 2]: Atk 229% x 3 Hits,
-[Lv 3]: Atk 242% x 3 Hits,
-[Lv 4]: Atk 255% x 3 Hits,
-[Lv 5]: Atk 268% x 3 Hits,
-[Lv 6]: Atk 281% x 3 Hits,
-[Lv 7]: Atk 294% x 3 Hits,
-[Lv 8]: Atk 307% x 3 Hits,
-[Lv 9]: Atk 320% x 3 Hits,
-[Lv 10]: Atk 333% x 3 Hits`,
+Description: Deals ranged P.DMG to the target, Knocking it back 3 cells.
+Allows stacking Triple Strafe Charging for 60s whenever Double Strafe is used. Damage scales with the stacks and AGI. VCT scales with skill level.
+Catalyst: 1x Arrow
+[Lv. 1]: ATK 216% x Hits. VCT: 0.33s SP Cost: 22
+[Lv. 2]: ATK 229% x Hits, VCT: 0.35s SP Cost: 24
+[Lv. 3]: ATK 242% x Hits. VCT: 0.37s SP Cost: 26
+[Lv. 4]: ATK 255% x Hits. VCT: 0.39s SP Cost: 28
+[Lv. 5]: ATK 268% x Hits. VCT: 0.42s SP Cost: 30
+[Lv. 6]: ATK 281% x Hits. VCT: 0.45s SP Cost: 32
+[Lv. 7]: ATK 294% x Hits. VCT: 0.48s SP Cost: 34
+[Lv. 8]: ATK 307% x Hits. VCT: 0.52s SP Cost: 36
+[Lv. 9]: ATK 320% x Hits, VCT: 0.56s SP Cost: 38
+[Lv.10]: ATK 333% x Hits. VCT: 0.60s SP Cost: 40
+Formula: ATK (%): 203 + (Skill Lv x 13) + (Triple Strafe Charging Stacks x AGI)) x Hits `,
     img: tripleStrafe,
   },
 ];
